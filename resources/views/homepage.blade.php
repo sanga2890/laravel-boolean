05c469e2eea406b8b16b7e89caf8735cfea0282d
Stefano <stefano2890@gmail.com>
2020-06-30 19:27:08 +0200

    added laravel, bootstrap and home route

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Boolean careers</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <h1>Boolean</h1>
+    </body>
+</html>
